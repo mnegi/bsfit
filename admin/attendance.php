5422639cb6107af32741e9c53ce34610b5d6f06f
Manohar Negi <manohar.negi@gmail.com>
2016-03-25 15:10:13 +0530

    student

--- admin/attendance.php
+++ admin/attendance.php
@@ -99,16 +99,16 @@
 		?>
 		
 		<!-- Theme Base, Components and Settings -->
-		<script src="assets/javascripts/theme.js"></script>
+		<script src="../assets/javascripts/theme.js"></script>
 		
 		<!-- Theme Custom -->
-		<script src="assets/javascripts/theme.custom.js"></script>
+		<script src="../assets/javascripts/theme.custom.js"></script>
 		
 		<!-- Theme Initialization Files -->
-		<script src="assets/javascripts/theme.init.js"></script>
+		<script src="../assets/javascripts/theme.init.js"></script>
 
 
 		<!-- Examples -->
-		<script src="assets/javascripts/dashboard/examples.dashboard.js"></script>
+		<script src="../assets/javascripts/dashboard/examples.dashboard.js"></script>
 	</body>
 </html>
